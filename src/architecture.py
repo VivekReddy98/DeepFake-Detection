b6ba9aefc16951c5b2b91f4c117106137c2d559d
Vivek Reddy Karri7 <vivekreddykarri98@gmail.com>
2020-03-30 02:02:23 -0400

    Commenting in architecture.py

--- src/architecture.py
+++ src/architecture.py
@@ -9,7 +9,7 @@ class DeepFakeDetector:
         '''
         Frames: According to the Paper the Frame size is set to 20, 40 or 80.
         Issue Regarding BatchSize and Frames: https://github.com/keras-team/keras/issues/8364
-        Very Important: PLEASE Evaluating or Predicting using the batch size as specified in the Training Phase
+        Very Important: While Evaluating or Predicting use the batch size as specified in the Training Phase
         '''
         self.FRAMES = frames
         pass
@@ -25,9 +25,10 @@ class DeepFakeDetector:
 
     def build(self, inception_path, verbose=False):
         '''
+        --------------------------------------------------------------------------
         Takes a frame of size 299*299*3 and returns a vector of shape 1*2048
         Pre-trained InceptionV3 is used as the CNN BackBone
-
+        ---------------------------------------------------------------------------
                              To save InceptionV3 weights
         #https://github.com/fchollet/deep-learning-models/blob/master/inception_v3.py
         model = InceptionV3(include_top=False, weights='imagenet', pooling='avg')
@@ -35,6 +36,26 @@ class DeepFakeDetector:
         for layer in model.layers:
             layer.trainable = False
         model.save(inception_path)
+        ---------------------------------------------------------------------------
+                             Trainable Model Architecture
+
+        Layer (type)                 Output Shape              Param #
+        =================================================================
+        time_distributed (TimeDistri (None, 80, 2048)          21802784
+        _________________________________________________________________
+        lstm (LSTM)                  (None, 2048)              33562624
+        _________________________________________________________________
+        dense (Dense)                (None, 512)               1049088
+        _________________________________________________________________
+        dropout (Dropout)            (None, 512)               0
+        _________________________________________________________________
+        dense_1 (Dense)              (None, 1)                 513
+        =================================================================
+        Total params: 56,415,009
+        Trainable params: 34,612,225
+        Non-trainable params: 21,802,784 (Inception V3 Parameters)
+        _________________________________________________________________
+
         '''
 
         CNN = tensorflow.keras.models.load_model(inception_path)
